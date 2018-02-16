a2a6a77996a6bb3dc83e1e5dd149f7e9ac99e2ee
Paul McCarthy <phantasos42@gmail.com>
2018-02-15 18:35:11 -0600

    find vue-js for index page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,25 +1,28 @@
 /* global Vue */
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function(event) {
   var app = new Vue({
-    el: '#app',
+    el: "#app",
     data: {
       leads: [],
       time_format: "12/25/17",
       url: "https://www.google.com/"
     },
     mounted: function() {
-      $.get('/api/v1/leads.json').success(function(response) {
-        console.log(this);
-        this.leads = response;
-      }.bind(this));
+      $.get("/api/v1/leads.json").success(
+        function(response) {
+          console.log(this);
+          this.leads = response;
+        }.bind(this)
+      );
     },
     methods: {
       moment: function(date) {
         return moment(date);
+      },
+      accordion: function() {
+        console.log("hello");
       }
     },
-    computed: {
-
-    },
+    computed: {}
   });
 });
