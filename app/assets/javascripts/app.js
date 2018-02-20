30f86e17c0a7aa301bf0119285ece6f281b84ac1
Paul McCarthy <phantasos42@gmail.com>
2018-02-19 18:13:34 -0600

    getting ready for pull

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,12 +2,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var app = new Vue({
     el: "#app",
-    data: {
-      leads: [],
-      time_format: "12/25/17",
-      url: "https://www.google.com/"
+    data: function() {
+      return {
+        leads: [],
+        time_format: "12/25/17",
+        url: "https://www.google.com/"
+      };
     },
-    mounted: function() {
+    created: function() {
       $.get("/api/v1/leads.json").success(
         function(response) {
           this.leads = response;
