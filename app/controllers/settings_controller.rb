8bb456636652f8b94b18b1a4297d8f9fc6b5a448
Paul McCarthy <phantasos42@gmail.com>
2018-02-26 19:12:10 -0600

    get form working

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,9 +1,25 @@
 class SettingsController < ApplicationController
   def index
     @settings_active = "active"
+    unless Setting.find_by(admin_id: 1) #hard coded as one because the site overall seems to support only one user
+      Setting.create(admin_id: 1)
+    end
+    @settings = Setting.find_by(admin_id: 1)
   end
 
   def update
-    redirect_to '/'
+    @settings = Setting.find_by(admin_id: 1)
+    new_content = params["settings"]["auto_text_content"]
+    @settings.auto_text_content = new_content
+    if @settings.save
+      redirect_to '/'
+    else
+      redirect_to '/settings/edit'
+
+    end
+  end
+
+  def setting_params
+    params.require(:setting).permit(:admin_id, :auto_text_content)
   end
 end
