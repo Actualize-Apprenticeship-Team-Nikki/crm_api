e577ce1d265ba2f93105fb1b5bd169ab240345f2
Jaewon Park <eliana.jaewon@gmail.com>
2018-03-05 18:38:54 -0600

    262 - Change phone number back to params

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -124,7 +124,7 @@ class LeadsController < ApplicationController
       @client = Twilio::REST::Client.new
       @client.messages.create(
         from: ENV['TWILIO_PHONE_NUMBER'],
-        to: '2243053960',
+        to: params[:phone],
         body: "Hi #{params[:first_name].split.first}! " + (current_admin.setting ? current_admin.setting.auto_text_content : "This is Rena from Actualize. Do you have a minute to chat?")
       )
       flash[:success] = "Auto text sent!"
