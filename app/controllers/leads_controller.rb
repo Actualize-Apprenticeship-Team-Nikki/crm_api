9a9ee17ff8eeaa70e9a0c82b8f5b3ff6dcf21de2
benjaminsinaiko <ben.jamin@outlook.com>
2018-03-08 19:58:46 -0600

    289 - Reverse text history

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -49,7 +49,7 @@ class LeadsController < ApplicationController
                   :to   => @lead.phone,
                   :from => ENV['TWILIO_PHONE_NUMBER']
     })
-    @messages = (messages_from_lead + messages_from_call_converter).sort_by {|m| m.date_sent}
+    @messages = (messages_from_lead + messages_from_call_converter).sort_by {|m| m.date_sent}.reverse
   end
 
   def update
