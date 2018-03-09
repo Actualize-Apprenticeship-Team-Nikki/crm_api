f698e8031b4017231c004f49f226a9e960c49590
Tyler Damme <tyler.damme@gmail.com>
2018-03-08 19:15:45 -0600

    Added leadsHelper method

--- app/helpers/leads_helper.rb
+++ app/helpers/leads_helper.rb
@@ -1,2 +1,12 @@
 module LeadsHelper
+  def convert_twilio(date)
+
+    date2 = DateTime.rfc2822(date).strftime("%Y").to_i
+
+    if date2 == DateTime.now.year
+      DateTime.rfc2822(date).strftime("%b %e, %l:%M%P")
+    else 
+      DateTime.rfc2822(date).strftime("(%Y) %b %e, %l:%M%P")
+    end
+  end
 end
