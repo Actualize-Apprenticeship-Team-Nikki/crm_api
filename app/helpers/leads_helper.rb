8343208f96397ce18cdc0e161efe21deccee2fbd
benjaminsinaiko <ben.jamin@outlook.com>
2018-03-15 18:49:21 -0500

    400 - Fixed text history utc datetime issue

--- app/helpers/leads_helper.rb
+++ app/helpers/leads_helper.rb
@@ -1,6 +1,6 @@
 module LeadsHelper
   def convert_twilio(date)
-    date2 = DateTime.rfc2822(date)
+    date2 = DateTime.rfc2822(date).to_time.in_time_zone("Central Time (US & Canada)")
 
     if date2.strftime("%Y").to_i == DateTime.now.year
       date2.strftime("%b %e, %l:%M%P")
